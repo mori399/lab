04b234bfd4574af0f9965ff764948be3adf5a01f
mori399 <21it1.makeenok.d@pdu.by>
2023-02-21 23:44:02 +0300

    uiersdt
    uiersdt

---
+++ ти/1/prikolna/prikolna/tree.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Xml.Linq;
+using System.Globalization;
+
+namespace HuffmanTest
+{
+    public class HuffmanTree
+    {
+        private List<Node> nodes = new List<Node>();
+        public Node Root { get; set; }
+        
+        public Dictionary<char, int> Frequencies = new Dictionary<char, int>();
+
+        public void Build(string source)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (!Frequencies.ContainsKey(source[i]))
+                {
+                    Frequencies.Add(source[i], 0);
+                }
+
+                Frequencies[source[i]]++;
+            }
+
+            foreach (KeyValuePair<char, int> symbol in Frequencies)
+            {
+                nodes.Add(new Node() { Symbol = symbol.Key, Frequency = symbol.Value });
+            }
+
+            while (nodes.Count > 1)
+            {
+                List<Node> orderedNodes = nodes.OrderBy(node => node.Frequency).ToList<Node>();
+
+                if (orderedNodes.Count >= 2)
+                {
+                    // берёт первые 2 элемента
+                    List<Node> taken = orderedNodes.Take(2).ToList<Node>();
+
+                    // создает родительский узел для объединения частот
+                    Node parent = new Node()
+                    {
+                        Symbol = '*',
+                        Frequency = taken[0].Frequency + taken[1].Frequency,
+                        Left = taken[0],
+                        Right = taken[1]
+                    };
+
+                    nodes.Remove(taken[0]);
+                    nodes.Remove(taken[1]);
+                    nodes.Add(parent);
+                }
+
+                this.Root = nodes.FirstOrDefault();
+
+            }
+
+        }
+
+        public BitArray Encode(string source)
+        {
+            List<bool> encodedSource = new List<bool>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                List<bool> encodedSymbol = this.Root.Traverse(source[i], new List<bool>());
+                encodedSource.AddRange(encodedSymbol);
+            }
+
+            BitArray bits = new BitArray(encodedSource.ToArray());
+
+            return bits;
+        }
+
+        public string Decode(BitArray bits)
+        {
+            Node current = this.Root;
+            string decoded = "";
+
+            foreach (bool bit in bits)
+            {
+                if (bit)
+                {
+                    if (current.Right != null)
+                    {
+                        current = current.Right;
+                    }
+                }
+                else
+                {
+                    if (current.Left != null)
+                    {
+                        current = current.Left;
+                    }
+                }
+
+                if (IsLeaf(current))
+                {
+                    decoded += current.Symbol;
+                    current = this.Root;
+                }
+            }
+
+            return decoded;
+        }
+        
+        public void printTable(BitArray bits)
+        {
+            Dictionary<char, string> chars = new Dictionary<char, string>();
+            Node current = this.Root;
+            string decoded = "";
+
+            string code = "";
+            foreach (bool bit in bits)
+            {
+                if (bit)
+                {
+                    if (current.Right != null)
+                    {
+                        current = current.Right;
+                        code += "1";
+                    }
+                }
+                else
+                {
+                    if (current.Left != null)
+                    {
+                        current = current.Left;
+                        code += "0";
+                    }
+                }
+
+                if (IsLeaf(current))
+                {
+                    decoded += current.Symbol;
+
+                    try { chars.Add(current.Symbol, code); }
+                    catch { }
+
+                    current = this.Root;
+                    code = "";
+                }
+            }
+
+            string p = decoded;
+            int length = 0;
+            foreach (char c in p)
+            {
+                length++;
+            }
+
+            List<double> aa = new List<double>();
+            foreach(var symb in chars)
+            {
+                int symb_c = 0;
+                foreach (char c in p)
+                {
+                if (c == symb.Key) symb_c++;
+                }
+                aa.Add(symb_c);
+               // Console.WriteLine(symb_c);
+            }
+
+            double qweqk = 0;
+            int n = 0;
+            foreach(var symb in chars)
+            {
+                qweqk = Math.Round(aa[n] / length, 4);
+                Console.WriteLine($"{symb.Key}\t code: {symb.Value} \t p: {qweqk} ");
+                n++;
+            }
+
+            Console.WriteLine("Entropy : " + chars.Count);
+        }
+
+        public bool IsLeaf(Node node)
+        {
+            return (node.Left == null && node.Right == null);
+        }
+
+    }
+}
---
+++ ти/лаба 1/Program.cs
@@ -0,0 +1,181 @@
+﻿static class Program
+{
+    static string Result = string.Empty;
+
+
+    private static void Main()
+    {
+        string n = File.ReadAllText("../../../save.txt");
+        Console.WriteLine($"Entropy: {Entropy(n)}");
+
+        Dictionary<char, int> letters = new();
+
+        for (int i = 0; i < n.Length; i++)
+        {
+            // Ищем уникальные символы
+            if (!letters.ContainsKey(n[i]))
+                // Добавляем в словарь, и считаем количество символов для приоритета
+                letters.Add(n[i], n.Count(x => x == n[i]));
+        }
+
+        // отдельный словарь для сортированных узлов
+        Dictionary<Node, int> sorted = new();
+        foreach (var c in letters.OrderBy(k => k.Value))
+        {
+            sorted.Add(new Node(c.Key), c.Value);
+        }
+
+        Dictionary<Node, int> saveNodes = new();
+
+        if (sorted.Count == 1)
+        {
+            var s = sorted.First();
+
+            s.Key.Code = "0";
+            saveNodes.Add(s.Key, s.Value);
+        }
+
+        while (sorted.Count != 1)
+        {
+            // Выделяем минимальные узлы, сохраняем и удаляем их
+            var left = MinBy(sorted);
+            sorted.Remove(left.Key);
+            var right = MinBy(sorted);
+            sorted.Remove(right.Key);
+
+            var temp = new Node('t');
+            temp.AddNeighbour(left.Key, right.Key);
+            sorted.Add(temp, left.Value + right.Value);
+
+            left.Key.AddToCode("1");
+            right.Key.AddToCode("0");
+
+            if (left.Key.Value is not null)
+            {
+                saveNodes.Add(left.Key, left.Value);
+            }
+
+            if (right.Key.Value is not null)
+            {
+                saveNodes.Add(right.Key, right.Value);
+            }
+
+            // Снова сортируем по возрастанию
+            sorted = SortByValue(sorted);
+        }
+
+        List<double> asd = new List<double>();
+        //int na = 0;
+        int dd = n.Length;
+
+        foreach (var c in letters)
+        {
+            int let_counter = 0;
+            foreach(char ch in n)
+            {
+                if (ch == c.Key) let_counter++;
+                //na++;
+            }
+            asd.Add(let_counter);
+            //let_counter = 0;
+        }
+
+        Dictionary<char, string> map = new();
+        foreach (var sn in saveNodes.OrderByDescending(x => x.Value))
+        {
+            map.Add(sn.Key.VAL, sn.Key.Code);
+        }
+
+        string res = String.Empty;
+        foreach (var v in n)
+        {
+            res += map[v];
+        }
+        Console.WriteLine($"Symbol\tCode\tLength\tProbability");
+        int j = 0;
+        foreach (var sn in saveNodes.OrderByDescending(x => x.Value))
+        {
+            Console.WriteLine($"{sn.Key.Value}\t{sn.Key.Code}\t{sn.Key.Code.Length}\t{Math.Round(asd[j]/dd, 4)}");
+            j++;
+        }
+
+        Console.WriteLine(res);
+
+
+        string decoded = String.Empty;
+
+        string token = string.Empty;
+        foreach (var ch in res)
+        {
+            token += ch;
+            if (map.ContainsValue(token))
+            {
+                decoded += map.FirstOrDefault(x => x.Value == token).Key;
+                token = String.Empty;
+            }
+        }
+
+        Console.WriteLine(decoded);
+
+    }
+
+    private class Node
+    {
+        public char? Value;
+
+        public Node? Left, Right;
+
+        public char VAL;
+        public string Code = string.Empty;
+
+        public Node(char value)
+        {
+            Value = value;
+            VAL = value;
+        }
+
+        public void AddNeighbour(Node left, Node right)
+        {
+            Value = null;
+            Left = left;
+            Right = right;
+        }
+
+        public void AddToCode(string dir)
+        {
+            Code = dir + Code;
+            Left?.AddToCode(dir);
+            Right?.AddToCode(dir);
+        }
+    }
+
+    private static Dictionary<Node, int> SortByValue(Dictionary<Node, int> letters)
+    {
+        Dictionary<Node, int> sorted = new();
+        foreach (var c in letters.OrderBy(k => k.Value))
+        {
+            sorted.Add(c.Key, c.Value);
+        }
+
+        return sorted;
+    }
+
+
+
+    private static KeyValuePair<Node, int> MinBy(Dictionary<Node, int> letters)
+    {
+        Dictionary<Node, int> sorted = new();
+        foreach (var c in letters.OrderBy(k => k.Value))
+        {
+            return new KeyValuePair<Node, int>(c.Key, c.Value);
+        }
+
+        return new KeyValuePair<Node, int>();
+    }
+
+    public static int Entropy(this string s)
+    {
+        HashSet<char> chars = new HashSet<char>(s);
+        return chars.Count;
+    }
+}

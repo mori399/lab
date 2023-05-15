b37481935e662292cc73e1de60eb9d74cc15d869
mori399 <21it1.makeenok.d@pdu.by>
2023-05-16 00:50:30 +0300

    ошибка страница не найдена

---
+++ ти/лаба 5/lab5/lab5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using lab5;
+
+var inputText = File.ReadAllText(@"..\source.txt");
+
+var key = Idea.CreateRandomKey(16);
+var ideaCrypt = new Idea(key, true);
+var encoded = ideaCrypt.Crypt(inputText.Select(x => (byte)x).ToArray());
+Console.WriteLine(Encoding.ASCII.GetString(encoded.ToArray()));
+
+Console.WriteLine("--------------");
+
+var ideaDecrypt = new Idea(key, false);
+var decoded = ideaDecrypt.Crypt(encoded);
+Console.WriteLine(Encoding.ASCII.GetString(decoded.ToArray()));
--- ти/лабабабабааб3/lab_tri/lab_tri/Program.cs
+++ ти/лабабабабааб3/lab_tri/lab_tri/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Numerics;
+﻿using System.Numerics;
 
 int ToInt(char c)
 {
